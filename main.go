7c2ebf3b89b1df1b2f8cabc512a7db10bc2a9c73
AK <ndrkrpv@gmail.com>
2025-08-18 10:40:39 +0300

    Format

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 	"strings"
 )
-
 
 func check(e error) {
 	if e != nil {
@@ -14,37 +13,32 @@ func check(e error) {
 	}
 }
 
-
 func main() {
 
 	data, err := os.ReadFile("./contacts.vcf")
 	check(err)
 
-
 	splitted := strings.Split(string(data), "\n")
 	var phone string
-
 
 	for i := 0; i < len(splitted); i++ {
 		if strings.HasPrefix(splitted[i], "TEL;") {
 			phoneStringSplitted := strings.Split(string(splitted[i]), ":")
 			phone = phoneStringSplitted[1]
-			if !strings.HasPrefix(phone, "+") && len(phone) == 13  {
+			if !strings.HasPrefix(phone, "+") && len(phone) == 13 {
 				phoneSplitted := strings.Split(phone, "-")
 				phoneRestored := "+38" + phoneSplitted[0] + phoneSplitted[1] + phoneSplitted[2]
 				phoneStringRestored := phoneStringSplitted[0] + ":" + phoneRestored
 				splitted[i] = phoneStringRestored
 			}
-			
-		}
-		
-	}
 
+		}
+
+	}
 
 	file, err := os.Create("./newcontacts.vcf")
 	check(err)
 	defer file.Close()
-
 
 	writer := bufio.NewWriter(file)
 	for _, line := range splitted {
